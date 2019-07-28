e3fc2844cc40c49fe2ea4889c128cdadc94cf4da
廣瀬大輝 <hirosedaiki@hirosedaikinoMacBook-Air.local>
2019-07-28 18:29:22 +0700

    Add Procfile + varchar = 191

--- database/migrations/2019_06_10_163702_create_users_table.php
+++ database/migrations/2019_06_10_163702_create_users_table.php
@@ -14,7 +14,7 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->unsignedbigIncrements('id');
+            $table->bigIncrements('id');
             $table->string('name')->nullable();
             $table->string('email');
             $table->string('password');
